17bf4d292489e279f65980da145406be9f5759f8
khushalbhavsar <khushalbhavsar41@gmail.com>
2026-01-26 22:42:34 +0530

    Commit

--- bookmyshow-app/src/index.js
+++ bookmyshow-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
@@ -13,8 +13,8 @@ ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
-        </BrowserRouter>
-      </Provider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
